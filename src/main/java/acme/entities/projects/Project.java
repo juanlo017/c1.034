c3ebf71ae8466bd99b2eb7aca02799f5f5e881f4
Juan Lopez <juanquiroslopez@gmail.com>
2024-02-20 12:13:54 +0100

    Project entity done

--- src/main/java/acme/entities/projects/Project.java
+++ src/main/java/acme/entities/projects/Project.java
@@ -1,44 +1,51 @@
 
-package acme.roles;
+package acme.entities.projects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.client.data.AbstractRole;
+import acme.client.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Manager extends AbstractRole {
+public class Project extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
 	private static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "^[A-Z]{3}-\\d{4}$", message = "{validation.project.code}")
+	private String				code;
 
 	@NotBlank
 	@Length(max = 75)
-	private String				degree;
+	private String				title;
 
 	@NotBlank
 	@Length(max = 100)
-	private String				overview;
+	private String				_abstract;
 
-	@NotBlank
-	@Length(max = 100)
-	private String				certifications;
+	boolean						indication;
+
+	@Digits(integer = 3, fraction = 2)
+	@Min(0)
+	private double				cost;
 
 	@URL
 	@Length(max = 255)
 	private String				link;
 
-	// Derived attributes -----------------------------------------------------
-
-	// Relationships ----------------------------------------------------------
 }
