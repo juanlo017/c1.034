f6932a0d21bcb3cc37421fbb6229ee104bd8fdfa
juanjunobr <juajunobr@alum.us.es>
2024-02-21 16:41:44 +0100

    Training module entity done

--- src/main/java/acme/entities/projects/TrainingModule.java
+++ src/main/java/acme/entities/projects/TrainingModule.java
@@ -10,33 +10,42 @@ import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 
+import acme.client.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class TrainingModule extends Project {
+public class TrainingModule extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
 
 	@NotBlank
 	@Column(unique = true)
 	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}", message = "{validation.project.code}")
-	private String			code;
+	private String				code;
 
 	@Past
-	private Date			creationMoment;
+	private Date				creationMoment;
 
 	@NotBlank
 	@Length(max = 101)
-	private String			details;
+	private String				details;
 
-	private DifficultyLevel	difficultyLevel;
+	private DifficultyLevel		difficultyLevel;
 
 	@Past
-	private Date			updateMoment;
+	private Date				updateMoment;
 
-	private String			optionalLink;
+	@URL
+	private String				optionalLink;
 
-	private Double			totalTime;
+	private Double				totalTime;
 }
