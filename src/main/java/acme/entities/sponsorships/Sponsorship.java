7416defde9eb183c93f323ae59025a2a7244891f
Pablo <pabmejbui@alum.us.es>
2024-03-01 12:47:16 +0100

    Sponsorship minor fix

--- src/main/java/acme/entities/sponsorships/Sponsorship.java
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -9,11 +9,13 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
@@ -47,9 +49,11 @@ public class Sponsorship extends AbstractEntity {
 
 	private TypeSponsorship		type;
 
+	@Email
 	private String				optionalEmail;
 
 	@URL
+	@Length(max = 255)
 	private String				optionalLink;
 
 
