8edb1475a6e1d2a4ea7de93c509c396c912cbc26
juanjunobr <juajunobr@alum.us.es>
2024-03-02 20:12:23 +0100

    Notice entity created

---
+++ src/main/java/acme/entities/Notice.java
@@ -0,0 +1,54 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Notice extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				author;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				message;
+
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+
+}
