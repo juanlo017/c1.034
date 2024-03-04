e66e927cf9a9b4b53eed2c88e65558b26456a7fe
joscasvaz <jicastrotic99@gmail.com>
2024-03-04 18:31:44 +0100

    feat: admin dashboards

---
+++ src/main/java/acme/forms/AdminDashboard.java
@@ -0,0 +1,39 @@
+
+package acme.forms;
+
+import acme.client.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminDashboard extends AbstractForm {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	int							numOfAuditors;
+	int							numOfConsumers;
+	int							numOfDevelopers;
+	int							numOfManagers;
+	int							numOfProviders;
+	int							numOfSponsors;
+	int							numOfClients;
+
+	double						ratioNonCriticalObjectives;
+	double						ratioNoticesWithMailAndLink;
+
+	double						avgRiskValue;
+	double						minRiskValue;
+	double						maxRiskValue;
+	double						deviationRiskValue;
+
+	double						avgClaimsPostedLast10Weeks;
+	double						minClaimsPostedLast10Weeks;
+	double						maxClaimsPostedLast10Weeks;
+	double						deviationClaimsPostedLast10Weeks;
+
+}
