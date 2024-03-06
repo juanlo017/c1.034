75f6991fc69c6d32bbcd849b8b73bdcd7d50e7be
Juan Lopez <juanquiroslopez@gmail.com>
2024-03-06 21:40:33 +0100

    Documentacion y cambios finales.

--- src/main/java/acme/entities/projects/Project.java
+++ src/main/java/acme/entities/projects/Project.java
@@ -4,7 +4,6 @@ package acme.entities.projects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.Transient;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -52,7 +51,6 @@ public class Project extends AbstractEntity {
 
 	// Derived attributes -----------------------------------------------------
 
-	@Transient
 	@PositiveOrZero
 	private int					cost;
 
