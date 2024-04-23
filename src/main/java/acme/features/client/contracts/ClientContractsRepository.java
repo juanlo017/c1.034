8651ad6b0599731618de49117c92f080c5fea6e4
joscasvaz <jicastrotic99@gmail.com>
2024-04-23 18:53:45 +0200

    feat: repo for clients and contracts

---
+++ src/main/java/acme/features/client/contracts/ClientContractsRepository.java
@@ -0,0 +1,25 @@
+
+package acme.features.client.contracts;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.contracts.Contract;
+import acme.roles.Client;
+
+@Repository
+public interface ClientContractsRepository extends AbstractRepository {
+
+	@Query("select c from Contract c where c.client.id = :id")
+	List<Contract> findContractsByClientId(int id);
+
+	@Query("select c from Contract c where c.id = :id")
+	Contract findContractById(int id);
+
+	@Query("select c from Client c where c.id = :id")
+	Client findClientById(int id);
+
+}
