c13085567aaab76657cecf3694bcfe3d922cb278
joscasvaz <jicastrotic99@gmail.com>
2024-04-23 18:55:09 +0200

    initial services commit

---
+++ src/main/java/acme/features/client/contracts/ClientContractsUpdateService.java
@@ -0,0 +1,13 @@
+
+package acme.features.client.contracts;
+
+import org.springframework.stereotype.Service;
+
+import acme.client.services.AbstractService;
+import acme.entities.contracts.Contract;
+import acme.roles.Client;
+
+@Service
+public class ClientContractsUpdateService extends AbstractService<Client, Contract> {
+
+}
