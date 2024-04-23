6d362dde06f2844253b582fef8c6ac7b63e8112c
joscasvaz <jicastrotic99@gmail.com>
2024-04-23 19:31:40 +0200

    fix: custom commands

--- src/main/java/acme/features/client/contracts/ClientContractsController.java
+++ src/main/java/acme/features/client/contracts/ClientContractsController.java
@@ -38,10 +38,10 @@ public class ClientContractsController extends AbstractController<Client, Contra
 
 	@PostConstruct
 	protected void initialise() {
-		super.addBasicCommand("list", this.listService);
+		super.addCustomCommand("list-mine", "list", this.listService);
 		super.addBasicCommand("show", this.showService);
 		super.addBasicCommand("create", this.createService);
-		super.addBasicCommand("publish", this.publishService);
+		super.addCustomCommand("publish", "update", this.publishService);
 		super.addBasicCommand("update", this.updateService);
 		super.addBasicCommand("delete", this.deleteService);
 	}
