5f58c23928a2bbf35c15d690a67e3e45b471539b
juanjunobr <juajunobr@alum.us.es>
2024-05-25 01:25:04 +0200

    Training module entity updated and create safe done

--- src/main/java/acme/entities/trainings/TrainingModule.java
+++ src/main/java/acme/entities/trainings/TrainingModule.java
@@ -9,6 +9,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -59,6 +61,8 @@ public class TrainingModule extends AbstractEntity {
 	private String				optionalLink;
 
 	@NotNull
+	@Min(1)
+	@Max(10000)
 	private Integer				totalTime;
 
 	private boolean				draftMode;
